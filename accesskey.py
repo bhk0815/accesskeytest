20b9b9d474a21208acbfa5d1fcb9c85bcac82796
bhk0815 <qhgusl88@gmail.com>
2026-01-05 22:25:23 +0900

    z

---
+++ accesskey.py
@@ -0,0 +1,6 @@
+# 절대로 권한이 있는 키를 사용하지 마세요!
+# 테스트용으로 생성한, 권한이 하나도 없는 사용자의 키여야 합니다.
+access_key = "AKIAQFNBMIQ4GY5SBGRL"
+secret_key = "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"
+
+print("This is a security test.")
