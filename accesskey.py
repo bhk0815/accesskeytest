e3fe0e0032570eeb3aaee21a2b3220a92b93bdd6
bhk0815 <123886041+bhk0815@users.noreply.github.com>
2026-01-05 17:56:57 +0900

    Delete accesskey.py

--- accesskey.py
+++
@@ -1,6 +0,0 @@
-# 절대로 권한이 있는 키를 사용하지 마세요!
-# 테스트용으로 생성한, 권한이 하나도 없는 사용자의 키여야 합니다.
-access_key = "AKIAQFNBMIQ4HYXY3LGB"
-secret_key = "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"
-
-print("This is a security test.")
